3205b67e8a84caea7309af97b850f3f75dd81223
ddresslerlegalplans <ddressler@legalplans.com>
2022-08-11 13:56:27 -0400

    change persist_docs macro to handle None

--- core/dbt/include/global_project/macros/adapters/persist_docs.sql
+++ core/dbt/include/global_project/macros/adapters/persist_docs.sql
@@ -24,10 +24,12 @@
 
 {% macro default__persist_docs(relation, model, for_relation, for_columns) -%}
   {% if for_relation and config.persist_relation_docs() and model.description %}
-    {% do run_query(alter_relation_comment(relation, model.description)) %}
+    {% set query = alter_relation_comment(relation, model.description) %}
+    {% do run_query(query) if query is not none %}
   {% endif %}
 
   {% if for_columns and config.persist_column_docs() and model.columns %}
-    {% do run_query(alter_column_comment(relation, model.columns)) %}
+    {% set query = alter_column_comment(relation, model.columns) %}
+    {% do run_query(query) if query is not none %}
   {% endif %}
 {% endmacro %}
